29073c31c5ed6c562c105950ad46511e09662693
RomainBOURGIN <romain.bourgin.pro@gmail.com>
2026-04-25 18:48:17 +0200

    fix: migrations auto

--- Program.cs
+++ Program.cs
@@ -35,6 +35,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
